eeb9fec77abe6c6b677116dd9196d05c447647fa
Didier Deroey <didierderoey.pro@gmail.com>
2025-03-21 15:12:00 +0100

    Update mdp.php

--- compte/mdp.php
+++ compte/mdp.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 
-$servername = "localhost";
+$servername = "mysql.railway.internal";
 $username = "root";
-$password = "";
-$dbname = "db_app";
+$password = "rglxfUPkHBidvHHeXYLLLckxROKYJMpB";
+$dbname = "railway";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
